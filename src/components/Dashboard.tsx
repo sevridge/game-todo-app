2c3e9c7c8e56281f482869bc121df08370fcb991
sevridge <247145022+sevridge@users.noreply.github.com>
2026-08-11 13:12:13 +0900

    feat: タスクを追加ボタンを追加

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import GameSelector from "./GameSelector";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function Dashboard() {
   // TODO: ゲームタスク一覧を取得する
   return (
     <>
       <GameSelector />
+      <div className="p-3">
+        <div className="flex flex-col gap-2">
+          <button className="flex items-center outline-none gap-1 border border-dashed p-2 rounded-md cursor-pointer">
+            <FontAwesomeIcon icon={faPlus} />
+            <div>タスクを追加</div>
+          </button>
+        </div>
+      </div>
     </>
   )
 }
