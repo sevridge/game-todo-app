46cbc3cf07b397f7bddf06a95d2f95c9cfa6653b
sevridge <247145022+sevridge@users.noreply.github.com>
2026-08-14 12:51:38 +0900

    feat: タスクコンポーネント追加

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import GameSelector from "./GameSelector";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useId } from "react";
+
+function TaskItem({task}: {task: StoreTask}) {
+  const id = useId();
+  return (
+    <div className="px-2 py-1 rounded-md bg-zinc-800 select-none">
+      <div className="flex items-center gap-2">
+        <label htmlFor={id} className="group">
+          <input type="checkbox" className="hidden" id={id} />
+          <div className="flex items-center justify-center w-4 h-4 rounded-sm cursor-pointer bg-zinc-600 group-has-[input:checked]:bg-zinc-500">
+            <div className="hidden group-has-[input:checked]:flex"><FontAwesomeIcon icon={faCheck} className="text-xs" /></div>
+          </div>
+        </label>
+        <div>{task.label}</div>
+      </div>
+    </div>
+  )
+}
 
 export default function Dashboard({regGames, regTasks}: {regGames?: StoreGame[], regTasks?: StoreTask[]}) {
   return (
@@ -8,7 +26,12 @@ export default function Dashboard({regGames, regTasks}: {regGames?: StoreGame[],
       <GameSelector regGames={regGames} />
       <div className="p-3">
         <div className="flex flex-col gap-2">
-          <button className="flex items-center outline-none gap-1 border border-dashed p-2 rounded-md cursor-pointer">
+          <div className="flex flex-col gap-2">
+            {regTasks?.map((v, i) => {
+              return <TaskItem key={i} task={v} />
+            })}
+          </div>
+          <button className="flex items-center outline-none gap-1 border border-dashed px-2 py-1 rounded-md cursor-pointer">
             <FontAwesomeIcon icon={faPlus} />
             <div>タスクを追加</div>
           </button>
