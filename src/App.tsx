5c7457ba5c06553772b21d2072fd5d22a2536d88
sevridge <247145022+sevridge@users.noreply.github.com>
2026-08-20 11:59:18 +0900

    feat: 更新時間を過ぎた場合にチェックを解除

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,69 @@ async function initWindow() {
   window.setPosition(new LogicalPosition(posX, 0));
 }
 
+async function isTaskReset(task: StoreTask) {
+  const tasksLastUpdate: {[id: string]: number}|undefined = await tasksStore.get('lastUpdate');
+  const currentTaskLastUpdate = tasksLastUpdate?.[task.id];
+  if (!currentTaskLastUpdate) return false;
+  const now = new Date();
+  const resetTime = new Date(now);
+  resetTime.setHours(task.resetTimeStatus.time, 0, 0, 0);
+  const lastUpdate = new Date(currentTaskLastUpdate);
+  if (task.resetTime === 'daily') {
+    if (now < resetTime) {
+      resetTime.setDate(resetTime.getDate() - 1);
+    }
+  } else if (task.resetTime === 'weekly') {
+    const targetDay = task.resetTimeStatus.week || 0;
+    const currentDay = now.getDay();
+
+    let diff = currentDay - targetDay;
+    if (diff < 0) {
+      diff += 7;
+    }
+
+    resetTime.setDate(now.getDate() - diff);
+  } else if (task.resetTime === 'monthly') {
+    const targetDay = task.resetTimeStatus.day || 1;
+    resetTime.setDate(targetDay);
+
+    if (now < resetTime) {
+      resetTime.setMonth(resetTime.getMonth() - 1);
+    }
+    // TODO: 日付指定がその日が存在しない日だった場合にどうするか
+  } else if (task.resetTime === 'every-week') {
+    const first = new Date(now.getFullYear(), now.getMonth(), 1); // 月初を取得
+    const firstDay = first.getDay(); // 月初曜日を取得
+    const targetDay = task.resetTimeStatus.week || 0; // 設定されている指定曜日を取得
+
+    let diff = targetDay - firstDay; // 月初から何日後が指定された曜日か
+    if (diff < 0) {
+      diff += 7;
+    }
+
+    resetTime.setDate(first.getDate() + diff); // リセット日を月初の指定曜日にする
+    // 今日の前の指定曜日を取得
+    let diffPrevious = resetTime.getDay() - targetDay; // 今日から何日前がリセット曜日になるか
+    if (diffPrevious < 0) {
+      diffPrevious += 7;
+    }
+    resetTime.setDate(resetTime.getDate() + diffPrevious); // 今日から一番最新のリセット曜日
+    // リセット曜日が基準日から偶数日になっているか
+    const diffTime = resetTime.getTime() - first.getTime();
+    const diffDay = diffTime / (1000 * 60 * 60 * 24); // 0, 7, 14, 21 ...
+    const diffWeek = diffDay / 7; // 0, 1, 0, 1 ...
+    // diffWeekが1だった場合は1週間前の日を取得する
+    if (diffWeek === 1) {
+      resetTime.setDate(resetTime.getDate() - 7); // 1週間前が別の月の可能性がないわけでもない
+    }
+  }
+
+  if (lastUpdate <  resetTime) {
+    return true;
+  }
+  return false;
+}
+
 function App() {
   const [ registeredTasks, setRegisteredTasks ] = useState<StoreTask[]|null>(null);
   const [ registeredGames, setRegisteredGames ] = useState<StoreGame[]|null>(null);
@@ -30,7 +93,16 @@ function App() {
         setRegisteredGames(games);
       }
       if (tasks) {
-        setRegisteredTasks(tasks);
+        const newTasks: StoreTask[] = await Promise.all(tasks.map(async (_task) => {
+          const task = {..._task};
+          const isReset = await isTaskReset(task);
+          if (isReset) {
+            task.checked = false;
+          }
+
+          return task;
+        }))
+        setRegisteredTasks(newTasks);
       }
     })();
   }, []);
