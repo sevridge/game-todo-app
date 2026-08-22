f29684704f6b84861eef39ccba56959063f14afd
sevridge <247145022+sevridge@users.noreply.github.com>
2026-08-22 11:26:49 +0900

    feat: 初期幅を変更/リサイズ無効

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ async function initWindow() {
   const windowInner = await window.innerSize();
   const monWidth = monitor?.workArea.size.width || 1920;
   const monHeight = monitor?.workArea.size.height || 1080;
-  const winWidth =  500; // windowInner.width;
+  const winWidth =  350; // windowInner.width;
   const winHeight = windowInner.height;
   window.setSize(new LogicalSize(winWidth, monHeight));
   const posX = monWidth - winWidth;
